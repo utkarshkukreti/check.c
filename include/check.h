40176bff684470b4bcc4f0f6d8131559476500a4
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-10-14 11:57:49 +0530

    Make `check_print_stats` a static function.
    Not sure why, but this fixed a "duplicate symbol" error when embedding
    this framework in a project.

--- include/check.h
+++ include/check.h
@@ -55,7 +55,7 @@ static void indent(int level) {
   }
 }
 
-void check_print_stats() {
+static void check_print_stats() {
   printf("\n%s  %s %d assertions, %d passed, %d skipped, %d failed.%s\n",
          check_failed ? red : check_skipped ? blue : green,
          check_failed ? cross : tick,
